5c96568e0a980e69a41326883d3224258118797d
Mell <mellboii@tutamail.com>
2026-07-11 01:12:05 -0300

    add ccredits

--- ba_data/python/bauiv1lib/credits.py
+++ ba_data/python/bauiv1lib/credits.py
@@ -126,211 +126,31 @@ class CreditsWindow(bui.MainWindow):
                 left_widget=bui.get_special_widget('back_button'),
             )
 
-        def _format_names(names2: Sequence[str], inset: float) -> str:
-            sval = ''
-            # measure a series since there's overlaps and stuff..
-            space_width = (
-                bui.get_string_width(' ' * 10, suppress_warning=True) / 10.0
-            )
-            spacing = 330.0
-            col1 = inset
-            col2 = col1 + spacing
-            col3 = col2 + spacing
-            line_width = 0.0
-            nline = ''
-            for name in names2:
-                # move to the next column (or row) and print
-                if line_width > col3:
-                    sval += nline + '\n'
-                    nline = ''
-                    line_width = 0
-
-                if line_width > col2:
-                    target = col3
-                elif line_width > col1:
-                    target = col2
-                else:
-                    target = col1
-                spacingstr = ' ' * int((target - line_width) / space_width)
-                nline += spacingstr
-                nline += name
-                line_width = bui.get_string_width(nline, suppress_warning=True)
-            if nline != '':
-                sval += nline + '\n'
-            return sval
-
-        sound_and_music = bui.Lstr(
-            resource=f'{self._r}.songCreditText'
-        ).evaluate()
-        sound_and_music = sound_and_music.replace(
-            '${TITLE}', "'William Tell (Trumpet Entry)'"
-        )
-        sound_and_music = sound_and_music.replace(
-            '${PERFORMER}', 'The Apollo Symphony Orchestra'
-        )
-        sound_and_music = sound_and_music.replace(
-            '${PERFORMER}', 'The Apollo Symphony Orchestra'
-        )
-        sound_and_music = sound_and_music.replace(
-            '${COMPOSER}', 'Gioacchino Rossini'
-        )
-        sound_and_music = sound_and_music.replace('${ARRANGER}', 'Chris Worth')
-        sound_and_music = sound_and_music.replace('${PUBLISHER}', 'BMI')
-        sound_and_music = sound_and_music.replace(
-            '${SOURCE}', 'www.AudioSparx.com'
-        )
-        spc = '     '
-        sound_and_music = spc + sound_and_music.replace('\n', '\n' + spc)
-        names = [
-            'HubOfTheUniverseProd',
-            'Jovica',
-            'LG',
-            'Leady',
-            'Percy Duke',
-            'PhreaKsAccount',
-            'Pogotron',
-            'Rock Savage',
-            'anamorphosis',
-            'benboncan',
-            'cdrk',
-            'chipfork',
-            'guitarguy1985',
-            'jascha',
-            'joedeshon',
-            'loofa',
-            'm_O_m',
-            'mich3d',
-            'sandyrb',
-            'shakaharu',
-            'sirplus',
-            'stickman',
-            'thanvannispen',
-            'virotic',
-            'zimbot',
-        ]
-        names.sort(key=lambda x: x.lower())
-        freesound_names = _format_names(names, 90)
-
-        try:
-            with open(
-                os.path.join(
-                    bui.app.env.data_directory,
-                    'ba_data',
-                    'data',
-                    'langdata.json',
-                ),
-                encoding='utf-8',
-            ) as infile:
-                translation_contributors = json.loads(infile.read())[
-                    'translation_contributors'
-                ]
-        except Exception:
-            logging.exception('Error reading translation contributors.')
-            translation_contributors = []
-
-        translation_names = _format_names(translation_contributors, 60)
-
-        # Need to bake this out and chop it up since we're passing our
-        # 65535 vertex limit for meshes..
-        # We can remove that limit once we drop support for GL ES2.. :-/
-        # (or add mesh splitting under the hood)
         credits_text = (
-            '  '
-            + bui.Lstr(resource=f'{self._r}.codingGraphicsAudioText')
-            .evaluate()
-            .replace('${NAME}', 'Eric Froemling')
+            bui.Lstr(resource=f'{self._r}.creatorsText').evaluate()
             + '\n'
-            '\n'
-            '  '
-            + bui.Lstr(resource=f'{self._r}.additionalAudioArtIdeasText')
-            .evaluate()
-            .replace('${NAME}', 'Raphael Suter')
             + '\n'
-            '\n'
-            '  '
-            + bui.Lstr(resource=f'{self._r}.soundAndMusicText').evaluate()
+            + bui.Lstr(resource=f'{self._r}.modelsText').evaluate()
             + '\n'
-            '\n' + sound_and_music + '\n'
-            '\n'
-            '     '
-            + bui.Lstr(resource=f'{self._r}.publicDomainMusicViaText')
-            .evaluate()
-            .replace('${NAME}', 'Musopen.com')
             + '\n'
-            '        '
-            + bui.Lstr(resource=f'{self._r}.thanksEspeciallyToText')
-            .evaluate()
-            .replace('${NAME}', 'the US Army, Navy, and Marine Bands')
+            + bui.Lstr(resource=f'{self._r}.specialThanksBombSquadText').evaluate()
             + '\n'
-            '\n'
-            '     '
-            + bui.Lstr(resource=f'{self._r}.additionalMusicFromText')
-            .evaluate()
-            .replace('${NAME}', 'The YouTube Audio Library')
             + '\n'
-            '\n'
-            '     '
-            + bui.Lstr(resource=f'{self._r}.soundsText')
-            .evaluate()
-            .replace('${SOURCE}', 'Freesound.org')
+            + bui.Lstr(resource=f'{self._r}.deltaruneText').evaluate()
             + '\n'
-            '\n' + freesound_names + '\n'
-            '\n'
-            '  '
-            + bui.Lstr(
-                resource=f'{self._r}.languageTranslationsText'
-            ).evaluate()
             + '\n'
-            '\n'
-            + '\n'.join(translation_names.splitlines()[:146])
-            + '\n'.join(translation_names.splitlines()[146:])
+            + bui.Lstr(resource=f'{self._r}.noProfitText').evaluate()
             + '\n'
-            '\n'
-            '  Shout Out to Awesome Mods / Modders / Contributors:\n\n'
-            '     BombDash ModPack\n'
-            '     TheMikirog & SoK - BombSquad Joyride Modpack\n'
-            '     Mrmaxmeier - BombSquad-Community-Mod-Manager\n'
-            '     Ritiek Malhotra \n'
-            '     Dliwk\n'
-            '     vishal332008\n'
-            '     itsre3\n'
-            '     Drooopyyy\n'
-            '\n'
-            '  Holiday theme vector art designed by Freepik\n'
-            '\n'
-            '  '
-            + bui.Lstr(resource=f'{self._r}.specialThanksText').evaluate()
             + '\n'
-            '\n'
-            '     Todd, Laura, and Robert Froemling\n'
-            '     '
-            + bui.Lstr(resource=f'{self._r}.allMyFamilyText')
-            .evaluate()
-            .replace('\n', '\n     ')
             + '\n'
-            '     '
-            + bui.Lstr(
-                resource=f'{self._r}.whoeverInventedCoffeeText'
-            ).evaluate()
-            + '\n'
-            '\n'
-            '  ' + bui.Lstr(resource=f'{self._r}.legalText').evaluate() + '\n'
-            '\n'
-            '     '
-            + bui.Lstr(resource=f'{self._r}.softwareBasedOnText')
-            .evaluate()
-            .replace('${NAME}', 'the Khronos Group')
-            + '\n'
-            '\n'
-            '                                       '
-            '                      www.ballistica.net\n'
+            + bui.Lstr(resource=f'{self._r}.waitingText').evaluate()
         )
 
         txt = credits_text
         lines = txt.splitlines()
         line_height = 20
 
-        scale = 0.55
+        scale = 0.6
         self._sub_width = min(700, width - 80)
         self._sub_height = line_height * len(lines) + 40
 
@@ -343,6 +163,7 @@ class CreditsWindow(bui.MainWindow):
 
         voffs = 0
         for line in lines:
+            x = 50 if uiscale is bui.UIScale.SMALL else 20
             bui.textwidget(
                 parent=container,
                 padding=4,
@@ -350,7 +171,7 @@ class CreditsWindow(bui.MainWindow):
                 scale=scale,
                 flatness=1.0,
                 size=(0, 0),
-                position=(0, self._sub_height - 20 + voffs),
+                position=(x, self._sub_height - 20 + voffs),
                 h_align='left',
                 v_align='top',
                 text=bui.Lstr(value=line),
