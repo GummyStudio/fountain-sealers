bfcfeaa35fb48cc7423bc281f41408a24e215c85
GummyBoi_YT <150540342+GummyBoiYT1@users.noreply.github.com>
2026-07-16 13:43:00 -0400

    you can search characters now

--- ba_data/python/bauiv1lib/characterpicker.py
+++ ba_data/python/bauiv1lib/characterpicker.py
@@ -14,6 +14,9 @@ if TYPE_CHECKING:
     from typing import Any, Sequence
 
 
+
+
+
 class CharacterPickerDelegate:
     """Delegate for character-picker."""
 
@@ -58,7 +61,6 @@ class CharacterPicker(PopupWindow):
         self._delegate = delegate
         self._transitioning_out = False
 
-        # make a list of spaz icons
         self._spazzes = spazappearance.get_appearances()
         self._spazzes.sort()
         self._icon_textures = [
@@ -96,6 +98,7 @@ class CharacterPicker(PopupWindow):
             (self._height - self._scroll_height) * 0.5,
         )
 
+        self.last_filter_txt = 'kjFdjknjfksnjd'
         # Creates our _root_widget.
         super().__init__(
             position=position,
@@ -106,6 +109,8 @@ class CharacterPicker(PopupWindow):
             focus_position=self._scroll_position,
             focus_size=(self._scroll_width, self._scroll_height),
         )
+
+        # deltes everhthing lol
 
         self._scrollwidget = bui.scrollwidget(
             parent=self.root_widget,
@@ -125,77 +130,29 @@ class CharacterPicker(PopupWindow):
             size=(self._sub_width, self._sub_height),
             background=False,
         )
-        index = 0
-        mask_texture = bui.gettexture('characterIconMask')
-        for y in range(rows):
-            for x in range(columns):
-                pos = (
-                    x * (button_width + 2 * button_buffer_h) + button_buffer_h,
-                    self._sub_height
-                    - (y + 1) * (button_height + 2 * button_buffer_v)
-                    + 12,
-                )
-                btn = bui.buttonwidget(
-                    parent=self._subcontainer,
-                    button_type='square',
-                    size=(button_width, button_height),
-                    autoselect=True,
-                    texture=self._icon_textures[index],
-                    tint_texture=self._icon_tint_textures[index],
-                    mask_texture=mask_texture,
-                    label='',
-                    color=(1, 1, 1),
-                    tint_color=tint_color,
-                    tint2_color=tint2_color,
-                    on_activate_call=bui.Call(
-                        self._select_character, self._spazzes[index]
-                    ),
-                    position=pos,
-                )
-                bui.widget(edit=btn, show_buffer_top=60, show_buffer_bottom=60)
-                if self._spazzes[index] == selected_character:
-                    bui.containerwidget(
-                        edit=self._subcontainer,
-                        selected_child=btn,
-                        visible_child=btn,
-                    )
-                
+        self._filter_text = bui.textwidget(
+            parent=self.root_widget,
+            position=(20, self._height - 45),
+            size=(self._width - 40, 35),
+            editable=True,
+            text='',
+            max_chars=32,
+            on_return_press_call=bui.Call(self._refresh_characters),
+        )
+        
                
-                # SPECIAL CASE:
-                # Vessel takes the name of the config file.
-                if self._spazzes[index] == 'Vessel':
-                    try:
-                        name = bui.app.classic.startup.gameconfig["SurveyChoices"][
-                            'vessel_name'
-                        ].lower().capitalize().strip()
-                    except Exception as e:
-                        print('WARN: Was unable to get Vessel Name.', e)
-                        # Fall back..
-                        name = bui.Lstr(
-                            translate=('characterNames', self._spazzes[index])
-                        )
-                else:
-                    name = bui.Lstr(
-                        translate=('characterNames', self._spazzes[index])
-                    )
-                bui.textwidget(
-                    parent=self._subcontainer,
-                    text=name,
-                    position=(pos[0] + button_width * 0.5, pos[1] - 12),
-                    size=(0, 0),
-                    scale=0.5,
-                    maxwidth=button_width,
-                    draw_controller=btn,
-                    h_align='center',
-                    v_align='center',
-                    color=(0.8, 0.8, 0.8, 0.8),
-                )
-                index += 1
+        
+        self._columns = 3
+        self._button_width = 100
+        self._button_height = 100
+        self._button_buffer_h = 10
+        self._button_buffer_v = 15
 
-                if index >= count:
-                    break
-            if index >= count:
-                break
+        self._tint_color = tint_color
+        self._tint2_color = tint2_color
+        self._selected_character = selected_character
+
+        self._character_widgets = []
         self._get_more_characters_button = btn = bui.buttonwidget(
             parent=self._subcontainer,
             size=(self._sub_width * 0.8, 60),
@@ -207,7 +164,191 @@ class CharacterPicker(PopupWindow):
             autoselect=True,
         )
         bui.widget(edit=btn, show_buffer_top=30, show_buffer_bottom=30)
+        self._active_tags = set()
+        
+        self._refresh_characters()
+       
+        self.tick_timer = bui.AppTimer(0.1, self._refresh_characters)
+    
+    def _toggle_tag(self, tag):
+        if tag in self._active_tags:
+            self._active_tags.remove(tag)
+        else:
+            self._active_tags.add(tag)
 
+       
+        
+    def _refresh_characters(self) -> None:
+        if self._transitioning_out:
+            self.tick_timer = None
+            return
+        else:
+            self.tick_timer = bui.AppTimer(0.1, self._refresh_characters)
+        
+        search = ""
+        if self._filter_text is not None:
+            search = (
+                bui.textwidget(query=self._filter_text)
+                .lower()
+                .strip()
+            )
+
+        if search == self.last_filter_txt:
+            # didnt do anything
+            return
+        self.last_filter_txt = search
+        
+
+        for widget in self._character_widgets:
+            try:
+                widget.delete()
+            except Exception:
+                pass
+        self._character_widgets.clear()
+
+
+
+        
+        
+        # get an evaluated lstr to make it feel better
+        # (causeeee it might be different across languaes)
+        translations = {}
+        for spaz in self._spazzes:
+            name = spaz
+            if spaz == "Vessel":
+                try:
+                    name = (
+                        bui.app.classic.startup.gameconfig[
+                            "SurveyChoices"
+                        ]["vessel_name"]
+                        .strip()
+                        .lower()
+                        .capitalize()
+                    )
+                except Exception:
+                    name = bui.Lstr(
+                        translate=("characterNames", spaz)
+                    ).evaluate()
+            else:
+                name = bui.Lstr(
+                    translate=("characterNames", spaz)
+                ).evaluate()
+            if name:
+                translations[spaz] = name
+
+        characters = [
+            c for c in self._spazzes
+            if search in translations[c].lower().strip()
+        ]
+      
+
+        columns = self._columns
+        rows = max(1, math.ceil(len(characters) / columns))
+        self._sub_height = (
+            5
+            + rows * (self._button_height + 2 * self._button_buffer_v)
+            + 100
+        )
+
+        bui.containerwidget(
+            edit=self._subcontainer,
+            size=(self._sub_width, self._sub_height),
+        )
+
+        mask_texture = bui.gettexture("characterIconMask")
+
+        for index, character in enumerate(characters):
+
+            row = index // columns
+            col = index % columns
+
+            pos = (
+                col * (self._button_width + 2 * self._button_buffer_h)
+                + self._button_buffer_h,
+
+                self._sub_height
+                - (row + 1)
+                * (self._button_height + 2 * self._button_buffer_v)
+                + 12,
+            )
+            try:
+
+                texture = bui.gettexture(
+                    bui.app.classic.spaz_appearances[
+                        character
+                    ].icon_texture
+                )
+
+                tint_texture = bui.gettexture(
+                    bui.app.classic.spaz_appearances[
+                        character
+                    ].icon_mask_texture
+                )
+            except:
+                continue
+            btn = bui.buttonwidget(
+                parent=self._subcontainer,
+                button_type="square",
+                size=(self._button_width, self._button_height),
+                position=pos,
+                texture=texture,
+                tint_texture=tint_texture,
+                mask_texture=mask_texture,
+                label="",
+                color=(1, 1, 1),
+                tint_color=self._tint_color,
+                tint2_color=self._tint2_color,
+                autoselect=True,
+                on_activate_call=bui.Call(
+                    self._select_character,
+                    character,
+                ),
+            )
+
+            if character == self._selected_character:
+                bui.containerwidget(
+                    edit=self._subcontainer,
+                    selected_child=btn,
+                    visible_child=btn,
+                )
+
+            # SPECIAL CASE:
+            # Vessel takes the name of the config file.
+            if character == "Vessel":
+                try:
+                    name = (
+                        bui.app.classic.startup.gameconfig[
+                            "SurveyChoices"
+                        ]["vessel_name"]
+                        .strip()
+                        .capitalize()
+                    )
+                except Exception:
+                    name = bui.Lstr(
+                        translate=("characterNames", character)
+                    )
+            else:
+                name = bui.Lstr(
+                    translate=("characterNames", character)
+                )
+
+            label = bui.textwidget(
+                parent=self._subcontainer,
+                text=name,
+                position=(
+                    pos[0] + self._button_width * 0.5,
+                    pos[1] - 12,
+                ),
+                size=(0, 0),
+                scale=0.5,
+                maxwidth=self._button_width,
+                draw_controller=btn,
+                h_align="center",
+                v_align="center",
+                color=(0.8, 0.8, 0.8, 0.8),
+            )
+
+            self._character_widgets.extend((btn, label))
     def _on_store_press(self) -> None:
         from bauiv1lib.account.signin import show_sign_in_prompt
 
@@ -232,6 +373,7 @@ class CharacterPicker(PopupWindow):
         if not self._transitioning_out:
             self._transitioning_out = True
             bui.containerwidget(edit=self.root_widget, transition='out_scale')
+
 
     @override
     def on_popup_cancel(self) -> None:
