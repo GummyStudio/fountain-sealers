d80bf85480e7027e73301d0c4896441c47b682ec
Mell <mellboii@tutamail.com>
2026-07-18 14:40:39 -0300

    add characters to inventory

--- ba_data/python/bauiv1lib/inventory.py
+++ ba_data/python/bauiv1lib/inventory.py
@@ -7,7 +7,58 @@ from __future__ import annotations
 from typing import override
 
 import bauiv1 as bui
+import bascenev1lib.actor.spazappearance as spazapp
+import random
 
+class AvatarViewWindow(bui.Window):
+    """Shows a character's avatar."""
+    def __init__(self, character):
+        self._width = 800
+        self._height = 800
+        scale = 0.7
+        super().__init__(
+            root_widget=bui.containerwidget(
+                size=(self._width, self._height),
+                toolbar_visibility='menu_minimal',
+                scale=scale,
+                transition='in_scale',
+            ),
+        )
+        image_sizeoff = 60
+        img_w = self._width - image_sizeoff
+        img_h = self._height - image_sizeoff
+        spaz = character
+        bui.imagewidget(
+            parent=self._root_widget,
+            position=(
+                (self._width - img_w) * 0.5,
+                (self._height - img_h) * 0.5,
+            ),
+            size=(img_w, img_h),
+            texture=bui.gettexture(spaz.icon_texture),
+            tint_texture=bui.gettexture(spaz.icon_mask_texture),
+            tint_color=spaz.default_color,
+            tint2_color=spaz.default_highlight,
+        )
+        btn = bui.buttonwidget(
+            parent=self._root_widget,
+            position=(-10, self._height - 40),
+            size=(60, 55),
+            scale=1.4,
+            label=bui.charstr(bui.SpecialChar.BACK),
+            button_type='backSmall',
+            extra_touch_border_scale=2.0,
+            autoselect=True,
+            on_activate_call=self.close,
+        )
+        
+        bui.containerwidget(edit=self._root_widget, cancel_button=btn)
+    
+    def close(self):
+        bui.containerwidget(
+            edit=self._root_widget,
+            transition='out_scale',
+        )
 
 class InventoryWindow(bui.MainWindow):
     """Shows what you got."""
@@ -22,11 +73,11 @@ class InventoryWindow(bui.MainWindow):
 
         assert bui.app.classic is not None
         uiscale = bui.app.ui_v1.uiscale
-        self._width = 1400 if uiscale is bui.UIScale.SMALL else 750
+        self._width = 1400 if uiscale is bui.UIScale.SMALL else 450
         self._height = (
             1200
             if uiscale is bui.UIScale.SMALL
-            else 530 if uiscale is bui.UIScale.MEDIUM else 600
+            else 500
         )
         # xoffs = 70 if uiscale is bui.UIScale.SMALL else 0
         # yoffs = -45 if uiscale is bui.UIScale.SMALL else 0
@@ -98,9 +149,10 @@ class InventoryWindow(bui.MainWindow):
             bui.containerwidget(edit=self._root_widget, cancel_button=btn)
 
         button_width = 300
+        yoffs -= 120
         self._player_profiles_button = btn = bui.buttonwidget(
             parent=self._root_widget,
-            position=(self._width * 0.5 - button_width * 0.5, yoffs - 200),
+            position=(self._width * 0.5 - button_width * 0.5, yoffs),
             autoselect=True,
             size=(button_width, 60),
             label=bui.Lstr(resource='playerProfilesWindow.titleText'),
@@ -109,16 +161,119 @@ class InventoryWindow(bui.MainWindow):
             textcolor=(0.75, 0.7, 0.8),
             on_activate_call=self._player_profiles_press,
         )
+        yoffs -= 20
         bui.textwidget(
             parent=self._root_widget,
-            position=(self._width * 0.5, yoffs - 250),
+            position=(
+                self._width * 0.5,
+                yoffs,
+            ),
             size=(0, 0),
-            text=bui.Lstr(resource='moreSoonText'),
-            scale=0.7,
-            maxwidth=self._width * 0.9,
+            text=bui.Lstr(resource='yourCharactersText'),
+            color=(0.8, 0.9, 1, 0.6),
+            scale=0.8,
+            maxwidth=self._width - 300,
             h_align='center',
             v_align='center',
         )
+        if uiscale is bui.UIScale.SMALL:
+            scroll_width = self._width * 0.5
+        else:
+            scroll_width = self._width - 140
+        scroll_height = 300
+        yoffs -= 10 + scroll_height
+        apps = spazapp.get_appearances()
+        app_btn_height = 80
+        app_btn_width = scroll_width - 15
+        sub_height = (app_btn_height + 8) * len(apps)
+        sub_width = scroll_width
+        app_btn_y = sub_height - app_btn_height
+        app_btn_xoffs = -3
+        self._scrollwidget = bui.scrollwidget(
+            parent=self._root_widget,
+            size=(scroll_width, scroll_height),
+            position=(self._width * 0.5 - scroll_width * 0.5, yoffs),
+            simple_culling_v=100.0,
+            capture_arrows=True,
+            border_opacity=0.4,
+            center_small_content_horizontally=True,
+        )
+        self._subcontainer = bui.containerwidget(
+            parent=self._scrollwidget,
+            size=(sub_width, sub_height),
+            background=False,
+            claims_left_right=False,
+        )
+        for spaz in apps:
+            first = False
+            if apps.index(spaz) == 0:
+                first = True
+            spaz = bui.app.classic.spaz_appearances[spaz]
+            name = bui.Lstr(translate=('characterNames', spaz.name))
+            btn = bui.buttonwidget(
+                parent=self._subcontainer,
+                position=(sub_width * 0.5 - app_btn_width * 0.5 + app_btn_xoffs, app_btn_y),
+                size=(app_btn_width, app_btn_height),
+                label='',
+                color=(0.55, 0.5, 0.6),
+                textcolor=(0.75, 0.7, 0.8),
+                on_activate_call=bui.WeakCall(self._play_random_voiceline, spaz),
+                enable_sound=False,
+            )
+            bui.textwidget(
+                parent=self._subcontainer,
+                position=(
+                    sub_width * 0.5 - app_btn_width * 0.5 + app_btn_xoffs + (app_btn_height), 
+                    app_btn_y + (app_btn_height * 0.5) + 20,
+                ),
+                size=(0, 0),
+                text=name,
+                draw_controller=btn,
+                scale=0.9,
+                maxwidth=app_btn_width - 120,
+                h_align='left',
+            )
+            bui.buttonwidget(
+                parent=self._subcontainer,
+                position=(
+                    sub_width * 0.5 - app_btn_width * 0.5 + app_btn_xoffs + 10, 
+                    app_btn_y + (27 * 0.5)
+                ),
+                size=(app_btn_height - 27, app_btn_height - 27),
+                label='',
+                color=(1, 1, 1),
+                texture=bui.gettexture(spaz.icon_texture),
+                tint_texture=bui.gettexture(spaz.icon_mask_texture),
+                tint_color=spaz.default_color,
+                tint2_color=spaz.default_highlight,
+                mask_texture=bui.gettexture('characterIconMask'),
+                on_activate_call=bui.WeakCall(self._show_avatar, spaz),
+                selectable=False,
+            )
+            if first:
+                bui.buttonwidget(
+                    edit=btn,
+                    up_widget=self._player_profiles_button,
+                )
+            app_btn_y -= app_btn_height + 5
+        
+    def _show_avatar(self, spaz):
+        # no-op if our underlying widget is dead or on its way out.
+        if not self._root_widget or self._root_widget.transitioning_out:
+            return
+        AvatarViewWindow(spaz)
+    
+    def _play_random_voiceline(self, spaz):
+        voicelines = [
+            spaz.jump_sounds,
+            spaz.attack_sounds,
+            spaz.impact_sounds,
+            spaz.death_sounds,
+            spaz.pickup_sounds,
+            spaz.fall_sounds,
+        ]
+        chosen = random.choice(voicelines)
+        bui.getsound(random.choice(chosen)).play()
 
     def _player_profiles_press(self) -> None:
         # pylint: disable=cyclic-import
